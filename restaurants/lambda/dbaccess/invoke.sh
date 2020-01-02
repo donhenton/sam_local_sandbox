5325a7b2ec7d7cf882d0c7101ed7d0571a6a633d
Don Henton <donhenton@gmail.com>
2020-01-02 13:53:44 -0600

    setting up for review delete

--- restaurants/lambda/dbaccess/invoke.sh
+++ restaurants/lambda/dbaccess/invoke.sh
@@ -1,4 +1,5 @@
 #sam local invoke DbAccessFunction --event ./events/getAllRestaurants.json  --profile=cdktest
 #sam local invoke DbAccessFunction --event ./events/postNewRestaurant.json  --profile=cdktest
 #sam local invoke DbAccessFunction --event ./events/getSingleRestaurant.json  --profile=cdktest
-sam local invoke DbAccessFunction --event ./events/deleteRestaurant.json  --profile=cdktest
+#sam local invoke DbAccessFunction --event ./events/deleteRestaurant.json  --profile=cdktest
+sam local invoke DbAccessFunction --event ./events/deleteReviewForRestaurant.json  --profile=cdktest
