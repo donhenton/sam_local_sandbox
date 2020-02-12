3ea5f73e10937ab4f517de5e8eedaa5245f3a555
Don Henton <donhenton@gmail.com>
2020-02-12 15:01:40 -0600

    adding fixes for calling cfn

--- apps/ssmReader/ssmReader/app.js
+++ apps/ssmReader/ssmReader/app.js
@@ -5,7 +5,7 @@ var cfnResponse = require('cfn-response');
 
 exports.lambdaHandler = async(event, context) => {
 
-    var responseBody = { statusCode: 500, Value: 'request failed' }
+    var responseData = {}
     var responseStatus = 'SUCCESS';
     var physical_id = uuidv4();
     if (event['RequestType'] === 'Create' || event['RequestType'] === 'Update') {
@@ -21,23 +21,24 @@ exports.lambdaHandler = async(event, context) => {
         }
         try {
             const data = await ssmClient.getParameter(parms).promise();
-            responseBody.statusCode = 200;
-            responseBody.Value = data.Parameter.Value;
+
+            responseData['Value'] = data.Parameter.Value;
         } catch (err) {
             if (err.code === 'ParameterNotFound') {
-                responseBody.statusCode = 404;
-                responseBody.Value = err.code;
+
+                responseData.Value = err.code;
+                responseStatus = 'FAILED'
             } else {
-                responseBody.Value = JSON.stringify(err)
+                responseData['Value'] = JSON.stringify(err)
             }
 
         }
 
 
     } else {
-        responseBody.Value = `failed because RequestType not proper type`;
-
+        responseData['Value'] = `failed because RequestType not proper type`;
+        responseStatus = 'FAILED';
     }
-    //   cfnResponse.send(event, context, responseStatus, responseData, physical_id)
-    return responseBody;
+    cfnResponse.send(event, context, responseStatus, responseData, physical_id)
+    return responseData;
 };
