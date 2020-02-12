3a65a4f4e0e44952e0f1dfbbdbc568370426bd7b
Don Henton <donhenton@gmail.com>
2020-02-12 14:52:51 -0600

    working on js based ssmReader

---
+++ apps/ssmReader/invoke.sh
@@ -0,0 +1,1 @@
+sam local invoke SSMReaderFunction --event real_event.json   --profile=testuser
---
+++ apps/ssmReader/lambda.js
@@ -0,0 +1,43 @@
+var AWS = require('aws-sdk');
+var ssmClient = new AWS.SSM();
+const uuidv4 = require('uuid/v4');
+var cfnResponse = require('./cfn-response');
+
+exports.handler = async(event, context) => {
+
+    var responseBody = { statusCode: 500, Value: 'request failed' }
+    var responseStatus = 'SUCCESS';
+    var physical_id = uuid4();
+    if (event['RequestType'] === 'Create' || event['RequestType'] === 'Update') {
+        var withDecryption = true;
+        var decryptRequest = event['ResourceProperties']['WithDecryption']
+        if (decryptRequest && (decryptRequest + "" === 'false')) {
+            withDecryption = false;
+        }
+
+        var parms = {
+            Name: event['ResourceProperties']['ParameterName'],
+            WithDecryption: withDecryption
+        }
+        try {
+            const data = await ssmClient.getParameter(parms).promise();
+            responseBody.statusCode = 200;
+            responseBody.Value = data.Parameter.Value;
+        } catch (err) {
+            if (err.code === 'ParameterNotFound') {
+                responseBody.statusCode = 404;
+                responseBody.Value = err.code;
+            } else {
+                responseBody.Value = JSON.stringify(err)
+            }
+
+        }
+
+
+    } else {
+        responseBody.Value = `failed because RequestType not proper type`;
+
+    }
+    //   cfnResponse.send(event, context, responseStatus, responseData, physical_id)
+    return responseBody;
+};
